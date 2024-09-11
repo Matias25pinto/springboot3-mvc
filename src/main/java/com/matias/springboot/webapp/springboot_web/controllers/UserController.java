243b385702896b5a14ba9602c28349d627f648a9
Matias Pinto <matias25pinto@gmail.com>
2024-09-10 21:17:06 -0400

    Usar listas en thymeleaf

--- src/main/java/com/matias/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/matias/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.matias.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.matias.springboot.webapp.springboot_web.models.User;
 
@@ -15,6 +20,7 @@ public class UserController {
     public String details(Model model){
 
         User user = new User("Matias", "Pinto");
+        //user.setEmail("matias25pinto@gmail.com");
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
@@ -26,5 +32,22 @@ public class UserController {
         model.put("name", "Matias");
         model.put("lastname", "Pinto");
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+        // List<User> users = new ArrayList<>();
+        model.addAttribute("title", "Lista de usuarios");
+    
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        List<User> users = Arrays.asList(
+            new User("user_1", "lastname_user1", "user1@email.com"),
+            new User("user_2", "lastname_user2"), 
+            new User("user_3", "lastname_user3"));
+        return users;
     }
 }
--- src/main/java/com/matias/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/matias/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.matias.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,5 +34,25 @@ public class UserRestController {
         body.put("title", "Hola Mundo Spring Boot Map");
         body.put("user", user);
         return body;
+    }
+
+    @GetMapping("/list")
+    public List<User> list(){
+
+        User user1 = new User("Matias", "Pinto");
+        User user2 = new User("Nelson", "Castelvi");
+        User user3 = new User("Tobias", "Rodriguez");
+        
+        //Forma clasica
+        //List<User> userList = new ArrayList<>(); 
+        //userList.add(user1);
+        // userList.add(user2);
+        // userList.add(user3);
+
+        //Forma usando un helpers
+        List<User> userList = Arrays.asList(user1, user2, user3);
+
+        return userList;
+
     }
 }
